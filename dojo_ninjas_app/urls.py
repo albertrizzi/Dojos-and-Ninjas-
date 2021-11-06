443bd8cdc64ce3ea1f12d81048716c9810ab290c
Albert Rizzi <albertrizzi@gmail.com>
2021-11-05 20:31:22 -0400

    Dojos and Ninjas with template

--- dojo_ninjas_app/urls.py
+++ dojo_ninjas_app/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    path('', views.index),
+    path('ninjas/create', views.create_ninja),
+    path('dojos/create', views.create_dojo),
 ]
